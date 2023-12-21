0360f920ddc893fd99b96cbad4a2c56608fa842c
beancurdv <beancurdv0@gmail.com>
2023-12-21 18:31:37 +0800

    project init

---
+++ app/src/main/java/com/beancurdv/techdojo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.beancurdv.techdojo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
