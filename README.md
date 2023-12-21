039157e90bcf37ec90f4b878a7899f1d3b9253c3
beancurdv <beancurdv0@gmail.com>
2023-12-21 19:44:52 +0800

    Compile well

---
+++ README.md
@@ -0,0 +1,1 @@
+https://xie.infoq.cn/article/46ccb9380c6e876e84b204ecc
---
+++ build-logic/convention/src/main/kotlin/LogPlugin.kt
@@ -0,0 +1,9 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class LogPlugin : Plugin<Project> {
+
+    override fun apply(target: Project) {
+
+    }
+}
