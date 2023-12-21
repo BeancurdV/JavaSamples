e9b1d35a4c252e9165d78a600a6bd2c7185a2d11
z69yuan <z69yuan@163.com>
2023-12-21 23:30:14 +0800

    第一个transform终于完成了

---
+++ build-logic/convention/src/main/kotlin/LogMethodVisitor.kt
@@ -0,0 +1,31 @@
+import org.objectweb.asm.MethodVisitor
+import org.objectweb.asm.Opcodes
+import org.objectweb.asm.commons.AdviceAdapter
+
+class LogMethodVisitor(
+    private val className:String,
+    nextMethodVisitor: MethodVisitor,
+    access: Int,
+    name: String?,
+    descriptor: String?,
+) : AdviceAdapter(Opcodes.ASM5,
+    nextMethodVisitor,access,name,descriptor) {
+
+    override fun onMethodEnter() {
+        // 往栈上增加两个局部变量
+        mv.visitLdcInsn("LogMethodVisitor")
+        mv.visitLdcInsn("enter:$className.$name")
+
+        mv.visitMethodInsn(Opcodes.INVOKESTATIC,
+            "android/util/Log",
+            "i",
+            "(Ljava/lang/String;Ljava/lang/String;)I",
+            false
+            )
+        super.onMethodEnter()
+    }
+
+    override fun onMethodExit(opcode: Int) {
+        super.onMethodExit(opcode)
+    }
+}
--- build-logic/convention/src/main/kotlin/LogPlugin.kt
+++ build-logic/convention/src/main/kotlin/LogPlugin.kt
@@ -1,3 +1,7 @@
+import com.android.build.api.instrumentation.FramesComputationMode
+import com.android.build.api.instrumentation.InstrumentationScope
+import com.android.build.api.variant.AndroidComponentsExtension
+import com.android.build.api.variant.Variant
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -10,6 +14,14 @@ class LogPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         log("====================start apply ===========")
         log("apply target: ${target.displayName}")
+        // 注册Transform
+        val androidComponentsExtension = target.extensions.getByType(AndroidComponentsExtension::class.java)
+        androidComponentsExtension.onVariants { variant: Variant ->
+            variant.instrumentation.apply {
+                transformClassesWith(LogTransform::class.java,InstrumentationScope.PROJECT) {}
+                setAsmFramesComputationMode(FramesComputationMode.COMPUTE_FRAMES_FOR_INSTRUMENTED_CLASSES)
+            }
+        }
         log("====================end apply ===========")
     }
 
